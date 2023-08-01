4399b4d168cd57f2c67e6a0fec44c9884860f396
Oscar <80750422+oggeorre@users.noreply.github.com>
2023-08-01 10:20:00 +0200

    Refactor

--- utils/packageUtil.js
+++ utils/packageUtil.js
@@ -6,8 +6,6 @@ const db = require("../services/db");
 const zipHelper = require("./zipUtil");
 const xml = {};
 
-getXmlTemplates();
-
 async function init() {
   await getXmlTemplates();
 }
@@ -43,45 +41,50 @@ async function getXmlTemplates() {
 }
 
 function reconstructPackagesJson(packages, baseUrl) {
-  return packages.map((p) => {
-    p = { ...p };
-    p.Published = `/Date(${new Date(p.Published).getTime()})/`;
-    p.LastUpdated = `/Date(${new Date(p.LastUpdated).getTime()})/`;
+  return packages.map((package) => {
     return {
       __metadata: {
-        uri: `${baseUrl}/Packages(Id='${p.Id}',Version='${p.Version}')`,
+        uri: `${baseUrl}/Packages(Id='${package.Id}',Version='${package.Version}')`,
         type: "NuGet.Server.DataServices.ODataPackage",
-        edit_media: `${baseUrl}/Packages(Id='${p.Id}',Version='${p.Version}')/$value`,
-        media_src: `${baseUrl}/package/${p.Id}/${p.Version}`,
+        edit_media: `${baseUrl}/Packages(Id='${package.Id}',Version='${package.Version}')/$value`,
+        media_src: `${baseUrl}/package/${package.Id}/${package.Version}`,
         content_type: "application/zip",
       },
-      ...p,
+      ...package,
+      Published: `/Date(${new Date(package.Published).getTime()})/`,
+      LastUpdated: `/Date(${new Date(package.LastUpdated).getTime()})/`,
     };
   });
 }
 
 function reconstructPackagesXML(packages, baseUrl) {
   return packages
-    .map((p) => {
-      p = { ...p };
-      p.Published = new Date(p.Published).toISOString();
-      p.LastUpdated = new Date(p.LastUpdated).toISOString();
+    .map((package) => {
+      package = { ...package };
+      package.Published = new Date(package.Published).toISOString();
+      package.LastUpdated = new Date(package.LastUpdated).toISOString();
 
-      let newStr = xml.templateContent;
-      newStr = replaceString(newStr, "@baseUrl", baseUrl);
+      let xmlPackageContent = xml.templateContent;
+      xmlPackageContent = replaceString(xmlPackageContent, "@baseUrl", baseUrl);
 
-      for (const prop in p) {
-        newStr = replaceString(newStr, `@${prop}`, p[prop]);
+      for (const prop in package) {
+        xmlPackageContent = replaceString(
+          xmlPackageContent,
+          `@${prop}`,
+          package[prop]
+        );
       }
 
-      const keys = newStr.match(/\@([^<]*)/g);
-      if (keys && keys.length != 0) {
-        for (const key of keys) {
-          newStr = replaceString(newStr, `${key}`, "");
-        }
+      const keys = xmlPackageContent.match(/\@([^<]*)/g);
+      if (!keys || keys.length === 0) {
+        return xmlPackageContent;
       }
 
-      return newStr;
+      for (const key of keys) {
+        xmlPackageContent = replaceString(xmlPackageContent, `${key}`, "");
+      }
+
+      return xmlPackageContent;
     })
     .join("");
 }
@@ -93,39 +96,38 @@ function reconstructJson(packages, baseUrl, returnSingle) {
         results: reconstructPackagesJson(packages, baseUrl),
       },
     };
-  } else {
-    if (packages && packages.length !== 0) {
-      return {
-        d: reconstructPackagesJson(packages, baseUrl)[0],
-      };
-    } else {
-      return {
-        d: {},
-      };
-    }
   }
+
+  if (packages && packages.length !== 0) {
+    return {
+      d: reconstructPackagesJson(packages, baseUrl)[0],
+    };
+  }
+
+  return {
+    d: {},
+  };
 }
 
 function reconstructXml(packages, baseUrl, returnSingle) {
   if (!returnSingle) {
-    let header = replaceString(xml.templateHeader, "@baseUrl", baseUrl);
-    header = replaceString(header, "@date", new Date());
+    let xmlHeader = replaceString(xml.templateHeader, "@baseUrl", baseUrl);
+    xmlHeader = replaceString(xmlHeader, "@date", new Date());
     return (
-      header + reconstructPackagesXML(packages, baseUrl) + xml.templateFooter
+      xmlHeader + reconstructPackagesXML(packages, baseUrl) + xml.templateFooter
     );
-  } else {
-    if (packages.length !== 0) {
-      return (
-        `<?xml version="1.0" encoding="utf-8" standalone="yes"?>` +
-        reconstructPackagesXML(packages, baseUrl)
-      );
-    } else {
-      return (
-        `<?xml version="1.0" encoding="utf-8" standalone="yes"?>` +
-        xml.errorMeta
-      );
-    }
   }
+
+  if (packages && packages.length !== 0) {
+    return (
+      `<?xml version="1.0" encoding="utf-8" standalone="yes"?>` +
+      reconstructPackagesXML(packages, baseUrl)
+    );
+  }
+
+  return (
+    `<?xml version="1.0" encoding="utf-8" standalone="yes"?>` + xml.errorMeta
+  );
 }
 
 function replaceString(str, key, value) {
@@ -137,7 +139,7 @@ function replaceString(str, key, value) {
   return str.replace(regex, "");
 }
 
-function getPackageSkelaton() {
+function getPackageSkeleton() {
   return {
     Id: "",
     Version: "",
@@ -187,12 +189,13 @@ async function createDbPackage(newPackage) {
 }
 
 async function getPackages(query) {
-  let searchTerm = query.searchTerm;
   let filter = query.$filter;
+
+  // Not implemented
+  let searchTerm = query.searchTerm;
   let orderby = query.$orderby;
   let skip = query.$skip;
   let top = query.$top;
-  let dbObj = {};
 
   try {
     const idMatch = filter.match(/'(.*)'/);
@@ -215,49 +218,56 @@ async function getPackages(query) {
   }
 }
 
-function parseNuspec(xml) {
+function nuspecXmlToJson(xml) {
   try {
-    const skelaton = getPackageSkelaton();
+    const skeleton = getPackageSkeleton();
 
-    const regex1 = new RegExp(
+    const dependencyRegex = new RegExp(
       `(?:.?)<dependency(?:\\s)id=\\"(.*)\\"(?:\\s)version=\\"(.*)\\"(?:\\s)\\/>(?:.?)`,
       "gim"
     );
 
-    const match1 = xml.toString().matchAll(regex1);
-    for (const m of match1) {
-      const id = m[1];
-      const version = m[2];
+    const dependencyMatches = xml.toString().matchAll(dependencyRegex);
+    for (const dependencyMatch of dependencyMatches) {
+      const id = dependencyMatch[1];
+      const version = dependencyMatch[2];
 
-      if (skelaton.Dependencies === "") skelaton.Dependencies += `${id}:`;
-      else skelaton.Dependencies += `|${id}:`;
+      if (skeleton.Dependencies === "") {
+        skeleton.Dependencies += `${id}:`;
+      } else {
+        skeleton.Dependencies += `|${id}:`;
+      }
 
-      if (version && version !== "") skelaton.Dependencies += `${version}:`;
+      if (version && version !== "") {
+        skeleton.Dependencies += `${version}:`;
+      }
     }
 
-    for (const prop in skelaton) {
-      const regex2 = new RegExp(
+    for (const prop in skeleton) {
+      const propRegex = new RegExp(
         `\\<(\\s*)(${prop})(\\s*)\\>(.*)\\<\\/(\\s*)(${prop})(\\s*)\\>`,
         "gi"
       );
 
-      const match2 = regex2.exec(xml);
-      if (match2 && match2.length >= 5) {
-        if (typeof skelaton[prop] === "boolean") {
-          skelaton[prop] = match2[4] == "true";
-        }
+      const propMatches = propRegex.exec(xml);
+      if (!propMatches || propMatches.length < 5) {
+        continue;
+      }
 
-        if (typeof skelaton[prop] === "string") {
-          skelaton[prop] = match2[4];
-        }
+      if (typeof skeleton[prop] === "boolean") {
+        skeleton[prop] = propMatches[4] == "true";
+      }
 
-        if (typeof skelaton[prop] === "number") {
-          skelaton[prop] = Number(match2[4]);
-        }
+      if (typeof skeleton[prop] === "string") {
+        skeleton[prop] = propMatches[4];
+      }
+
+      if (typeof skeleton[prop] === "number") {
+        skeleton[prop] = Number(propMatches[4]);
       }
     }
 
-    return skelaton;
+    return skeleton;
   } catch (err) {
     console.error(err);
     return null;
@@ -293,51 +303,51 @@ async function getNugetPackageJson(file) {
 
   if (await fsHelper.exist(file)) {
     try {
-      const xml = await zipHelper.getNuspec(file);
-      const newPackage = parseNuspec(xml);
-      if (newPackage) {
-        newPackage.PackageHash = await fsHelper.getFileHash(file);
-        newPackage.PackageSize = (await fsHelper.getStats(file)).size;
-        if (newPackage.Version.includes("-")) {
-          newPackage.IsPrerelease = true;
+      const nuspecXml = await zipHelper.getNuspec(file);
+      const nuspecJson = nuspecXmlToJson(nuspecXml);
+      if (nuspecJson) {
+        nuspecJson.PackageHash = await fsHelper.getFileHash(file);
+        nuspecJson.PackageSize = (await fsHelper.getStats(file)).size;
+        if (nuspecJson.Version.includes("-")) {
+          nuspecJson.IsPrerelease = true;
         }
 
-        newPackage.NormalizedVersion = normalizeVersion(newPackage.Version);
+        nuspecJson.NormalizedVersion = normalizeVersion(nuspecJson.Version);
 
         const { latestVersion, absoluteLatestVersion } = await getLatestVersion(
-          newPackage.Id,
-          [newPackage.Version]
+          nuspecJson.Id,
+          [nuspecJson.Version]
         );
 
-        if (latestVersion === newPackage.Version) {
-          newPackage.IsLatestVersion = true;
+        if (latestVersion === nuspecJson.Version) {
+          nuspecJson.IsLatestVersion = true;
 
           await db.dbQuery(
             {
-              Id: newPackage.Id,
+              Id: nuspecJson.Id,
               repoUID,
             },
             db.queries.set.packageLatestVersionToFalse
           );
         }
 
-        if (absoluteLatestVersion === newPackage.Version) {
-          newPackage.IsAbsoluteLatestVersion = true;
+        if (absoluteLatestVersion === nuspecJson.Version) {
+          nuspecJson.IsAbsoluteLatestVersion = true;
 
           await db.dbQuery(
             {
-              Id: newPackage.Id,
+              Id: nuspecJson.Id,
               repoUID,
             },
             db.queries.set.packageAbsoluteLatestVersionToFalse
           );
         }
 
-        const date = Date.now();
-        newPackage.Published = date;
-        newPackage.LastUpdated = date;
+        const dateNow = Date.now();
+        nuspecJson.Published = dateNow;
+        nuspecJson.LastUpdated = dateNow;
 
-        return newPackage;
+        return nuspecJson;
       } else {
         new Error("new package json file is empty");
       }
@@ -351,15 +361,15 @@ async function getNugetPackageJson(file) {
   }
 }
 
-async function getLatestVersion(Id, extraVersionsArray) {
-  let versions = (
-    await db.dbQuery(
-      {
-        Id: Id,
-      },
-      db.queries.get.packageById
-    )
-  ).map((x) => x.Version);
+async function getLatestVersion(id, extraVersionsArray) {
+  const dbPackages = await db.dbQuery(
+    {
+      Id: id,
+    },
+    db.queries.get.packageById
+  );
+
+  let versions = dbPackages.map((x) => x.Version);
 
   if (extraVersionsArray) {
     versions = versions.concat(extraVersionsArray);
@@ -370,16 +380,22 @@ async function getLatestVersion(Id, extraVersionsArray) {
   let isPrerelease = false;
 
   for (const version of versions) {
-    if (version.indexOf("-") !== -1) isPrerelease = true;
-    else isPrerelease = false;
+    isPrerelease = version.indexOf("-") !== -1;
 
     if (compareVersions(version, absoluteLatestVersion) >= 0) {
       absoluteLatestVersion = version;
-      if (!isPrerelease) latestVersion = version;
-    } else if (!latestVersion && !isPrerelease) latestVersion = version;
+
+      if (!isPrerelease) {
+        latestVersion = version;
+      }
+    } else if (!latestVersion && !isPrerelease) {
+      latestVersion = version;
+    }
   }
 
-  if (!latestVersion) latestVersion = absoluteLatestVersion;
+  if (!latestVersion) {
+    latestVersion = absoluteLatestVersion;
+  }
 
   return {
     latestVersion,
